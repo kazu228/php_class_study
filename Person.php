1f8ce5903360dd34c72afb20b4674e596552a8e7
Kazu <kskkm0228@gmail.com>
2023-12-12 23:52:36 +0900

    コンストラクタ

--- Person.php
+++ Person.php
@@ -3,7 +3,13 @@
 class Person {
     public $firstName;
     public $lastName;
+    // コンストラクタ
+    public function __construct($firstName, $lastName) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
 
+    
     public function show () {
         print "僕の名前は、{$this->firstName } {$this->lastName } です、";
     }
---
+++ construct.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'Person.php';
+
+$p = new Person('山田', 'ぺこ');
+
+$p->show();
+
+
+
